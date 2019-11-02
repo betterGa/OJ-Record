e1cbb3bf270a47766c9bdef72fa631ac3820d333
yourjia <2086543608@qq.com>
2019-11-02 20:17:18 +0800

    1102

--- src/main/java/Search/DeepthFirstSearch/NumberOfIslands/Solution.java
+++ src/main/java/Search/DeepthFirstSearch/NumberOfIslands/Solution.java
@@ -3,25 +3,27 @@ package Search.DeepthFirstSearch.NumberOfIslands;
 public class Solution {
     int time;
     public int numIslands(char[][] grid) {
+        if(grid==null||grid.length==0) return 0;
         int line=grid.length;
         int column=grid[0].length;
         //遍历数组，遇到"1"进行深度优先遍历
         for(int i=0;i<line;i++)
             for(int j=0;j<column;j++)
             {
-                if(grid[i][j]=='1') dfs(grid,i,j);
+                if(grid[i][j]=='1') {time++;dfs(grid,i,j);}
             }
-return time;
+        return time;
     }
     public void dfs(char[][] data,int line,int column)
     {
-        if(line<0||line>data.length||column<0||column>data[0].length||data[line][column]!='1')
+        if(line<0||line>=data.length||column<0||column>=data[0].length||data[line][column]!='1')
             return;
+        data[line][column]='0';
+
         dfs(data,line-1,column);
         dfs(data,line+1,column);
         dfs(data,line,column-1);
         dfs(data,line,column+1);
-time++;
-return;
+        return;
     }
 }
