c788327f5b6849b9cac58422b3140453bee813dc
yourjia <2086543608@qq.com>
2019-11-16 20:57:12 +0800

    20191116

--- src/test/java/Search/BreadthFirstSearch/NaryTreeLevelOrderTraversal/SolutionTest.java
+++ src/test/java/Search/BreadthFirstSearch/NaryTreeLevelOrderTraversal/SolutionTest.java
@@ -1,5 +1,33 @@
+package Search.BreadthFirstSearch.NaryTreeLevelOrderTraversal;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class SolutionTest {
 
+    Solution solution=new Solution();
+    List<Node> rootlist=new ArrayList();
+    List<Node> list1=new ArrayList();
+    List<Node> list2=new ArrayList<>();
+Node node2=new Node(2,null);
+Node node3=new Node(4,null);
+Node node4=new Node(5,null);
+Node node5=new Node(6,null);
+    @Test
+    public void levelOrder() {
+        Node node1=new Node(3,list2);
+        list1.add(node1);
+        list1.add(node2);
+        list1.add(node3);
+        list2.add(node4);
+        list2.add(node5);
+        Node root=new Node(1,list1);
+
+        System.out.println(solution.levelOrder(root));
+    }
 }
--- src/test/java/Search/BreadthFirstSearch/RottingOrange/SolutionTest.java
+++ src/test/java/Search/BreadthFirstSearch/RottingOrange/SolutionTest.java
@@ -1,5 +1,23 @@
+package Search.BreadthFirstSearch.RottingOrange;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class SolutionTest {
-
+Solution solution=new Solution();
+    @Test
+    public void orangesRotting() {
+        int[][]grid=new int[][]{{1},{2},{1},{2}};
+        int result=solution.orangesRotting(grid);
+        Assert.assertEquals(1,result);
+    }
+    @Test
+    public void test1()
+    {int size = Integer.MAX_VALUE;
+        System.out.println(size);
+        size++;
+        System.out.println(size);
+    }
 }
