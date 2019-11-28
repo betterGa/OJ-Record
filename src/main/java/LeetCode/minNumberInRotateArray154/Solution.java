85faa5b5c1dd5dab4c7529de442541a5efb35c64
yourjia <2086543608@qq.com>
2019-11-28 09:34:56 +0800

    20191128 LeetCode 154:寻找旋转数组中最小元素

--- src/main/java/LeetCode/minNumberInRotateArray154/Solution.java
+++ src/main/java/LeetCode/minNumberInRotateArray154/Solution.java
@@ -1,4 +1,30 @@
 package LeetCode.minNumberInRotateArray154;
 
+//https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array-ii/comments/
+
 public class Solution {
-}
+    public int findMin(int[] nums) {
+        if(nums==null||nums.length==0)
+        {return 0;}
+        if(nums.length==1) return nums[0];
+        int head=0;
+        int tail=nums.length-1;
+
+        while (head!=tail)
+        {
+            int mid=(head+tail)/2;
+            if(nums[head]>nums[tail])
+            {
+                if(head+1==tail) return nums[tail];
+                else  head=mid;
+            }
+            else if(nums[head]<nums[tail])
+            {tail=head;
+                head=0;}
+            else
+            {if(head>=1&&nums[head]>nums[head-1]) {head--;}
+            else
+            {head++;}
+            }}
+        return nums[head];
+    }}
